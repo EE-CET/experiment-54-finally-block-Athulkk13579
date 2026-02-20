19fa517ca3cd9a604323e1a2106f9e464b1e9bf7
Athulkk13579 <athulkrishnak8086@gmail.com>
2026-02-20 10:25:52 +0530

    Update FinallyDemo.java

--- src/FinallyDemo.java
+++ src/FinallyDemo.java
@@ -1,17 +1,29 @@
 import java.util.Scanner;
 
-public class FinallyDemo {
-    
-        // TODO: Read a and b
-        
-        // TODO: try block
-        // Perform division
-        // Print "Result: " + result
-        
-        // TODO: catch ArithmeticException e
-        // Print "Error: " + e.getMessage()
-        
-        // TODO: finally block
-        // Print "Finally block executed"
+class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
+
+public class AgeValidation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        if (scanner.hasNextInt()) {
+            int age = scanner.nextInt();
+
+            try {
+                if (age < 18) {
+                    throw new InvalidAgeException("Not eligible to vote");
+                } else {
+                    System.out.println("Eligible to vote");
+                }
+            } catch (InvalidAgeException e) {
+                System.out.println("Exception: " + e.getMessage());
+            }
+        }
+
+        scanner.close();
     }
 }
